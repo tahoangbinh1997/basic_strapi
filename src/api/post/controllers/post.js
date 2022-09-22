d9c436c0c912cab6801d382b9e27242418e7ac52
binhth <binhth@hocmai.vn>
2022-09-22 16:29:59 +0700

    feat: add detail by slug

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
- * post controller
+ *  post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::post.post", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(post);
+
+    console.log(sanitizedEntity);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
